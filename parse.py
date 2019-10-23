25e77b145170eb23422eabac6f5b5fcf1da7be0c
Alec <Alec.m.williams-1@ou.edu>
2019-10-23 14:48:35 -0500

    slightly mor development
    tkinter sounds like it isnt a python window manager but apparently the internet says it is

--- parse.py
+++ parse.py
@@ -1,12 +1,22 @@
-file = open("data/pac_example.txt", "r")
-text = file.read()
-array = text.split('\n')
-#print(array)
-for x in range(0, array.__len__()):
-    array[x] = array[x].split('|,')
-    for y in array[x]:
-        #print (y)
-        y = y.strip('|')
-        #print (y)
-print(array)
+import tkinter      #hopefully this should be a window manager for python
+def __main__():
+    #opens and saves the contents of a file to 'text' variable
+    file = open("data/pac_example.txt", "r")
+    text = file.read()
+
+
+    #splits 'text' and stores in an array
+    array = text.split('\n')
+
+    #a vague attempt to loop through and parse the array even further
+    for x in range(0, array.__len__()):
+        array[x] = array[x].split('|,')
+        for y in array[x]:
+            # print (y)
+            y = y.strip('|')
+            # print (y)
+    #classic print statement debugging
+    print(array)
+
+
 
